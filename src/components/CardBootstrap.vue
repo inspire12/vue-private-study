e61028df65dc9dcc64d8b5a7705cff0cb0acdb78
inspire12 <seo.yeonghak.1992@gmail.com>
2018-12-28 17:08:39 +0900

    수정 Form 형태로

---
+++ src/components/CardBootstrap.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="test">
+  <b-card title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2">
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </p>
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+</div>
+
+</template>
+
+
+<style>
+  @import './test.css';
+</style>
+<script>
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Import the individual components
+import bCard from 'bootstrap-vue/es/components/card/card';
+import bCardHeader from 'bootstrap-vue/es/components/card/card-header';
+import bCardBody from 'bootstrap-vue/es/components/card/card-body';
+import bCardFooter from 'bootstrap-vue/es/components/card/card-footer';
+import bCardImage from 'bootstrap-vue/es/components/card/card-footer';
+import bTable from 'bootstrap-vue/es/components/table/table';
+
+
+// // Add components globally:
+Vue.component('b-card', bCard);
+Vue.component('b-card-header', bCardHeader);
+Vue.component('b-card-body', bCardBody);
+Vue.component('b-card-footer', bCardFooter);
+Vue.component('b-card-img', bCardImage);
+Vue.component('b-table', bTable);
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    // bCard,
+    // bCardHeader,
+    // bCardBody,
+    // bCardFooter,
+    // bCardImg,
+    // bTable
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="container">
+        <form @submit.prevent="addSkill">
+            <input type="text" placeholder="Enter a skill you have.." v-model="skill"> 
+            <p>{{skill}} </p>
+            <input type="checkbox" id="checkbox" v-model="checked">
+        </form>
+        <div class="holder">
+            <ul>
+                <li v-for="(data, index) in skills" :key="index">
+                    {{index}}. {{data.skill}}
+                </li>
+            </ul>
+        </div>
+
+        <p>These are the skills that you possess.</p>
+        <div v-bind:class="{alert:showAlert}">   </div>
+
+        <div v-bind:style="alertObject"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            skill: '',
+            skills : [
+                {"skill": "Vue.js"},
+                {"skill": "Frontend Developer"}
+            ],
+            showAlert: true,
+            alertObject: {
+                backgroundColor: 'yellow',
+                width: '100%',
+                height: '30px'
+            },
+            checked: false,
+            skill: ''
+        }
+    },
+    methods : {
+        addSkill(){
+            this.skills.push({skill: this.skill});
+            this.skill='';
+            console.log('The checkbox value is: '+this.checked);
+        }
+    }
+}
+</script>
+
+<style scoped>
+input {
+    width: calc(100%);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em; 
+    background-color: #323333;
+    /* 글자색  */
+    color: #687F7F; 
+}
+    .alert{
+        background: yellow;
+        font-weight: bold;
+        display: inline-block;
+        padding: 5px;
+        margin-top: -20px;
+        width: 100%;
+        height: 30px;
+    }
+    .holder {
+        background: #fff;
+    }
+    ul{
+        margin: 0;
+        padding: 0;
+        list-style-type: none; 
+    }    
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+    background: white;
+  }
+
+
+</style>
